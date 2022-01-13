051fe45597fb067a78e1c5e4cd67e711d2450d99
guosongqing <1084737312@qq.com>
2022-01-13 16:24:06 +0800

    dsd

--- pages/about/about.vue
+++ pages/about/about.vue
@@ -1,8 +1,10 @@
 <template>
 	<view>
-		about
-
-		<button type="default" @click="navto($event,1)" data-navname="my">hhh</button>
+		<!-- <button type="default" @click="navto($event,1)" data-navname="my">hhh</button> -->
+		<!-- <image src="../../static/indexcssp.png" mode="widthFix" style="width: 100%;height: 100%;"></image> -->
+		<view class="aa">
+			
+		</view>
 	</view>
 </template>
 
@@ -20,5 +22,11 @@
 </script>
 
 <style lang="scss">
-
+.aa{
+	display: inline-block;
+	background: url(https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.jj20.com%2Fup%2Fallimg%2F1114%2F0G320103935%2F200G3103935-1-1200.jpg&refer=http%3A%2F%2Fpic.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1643181341&t=b32b9d3e34af8789c3f25c29c94c08aa) no-repeat;
+	background-position: -150rpx -150rpx;
+	width: 150rpx;
+	height: 150rpx;
+}
 </style>
